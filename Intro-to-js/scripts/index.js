3e77c7b6df38f1b87cd08f0b3465ecfa26517da1
Andrei Kudrin <andreikudrin@gmail.com>
2020-07-23 18:38:14 +0100

    JS Project 1 - Accessing DOM Elements in JavaScript

--- Intro-to-js/scripts/index.js
+++ Intro-to-js/scripts/index.js
@@ -1,2 +1,14 @@
 var ResultContainer = document.getElementById("ResultContainer");
 ResultContainer.innerHTML = "Setting up the environment!";
+
+var header = document.getElementsByTagName("h1");
+var body = document.getElementById("ResultContainer");
+var footer = document.getElementsByClassName("Footer");
+
+header[0].textContent = "Header Text from JS";
+header[0].setAttribute("isHeader", "True");
+header[0].style.border = "2px solid black";
+console.log(header[0]);
+console.log(header);
+console.log(body);
+console.log(footer);
