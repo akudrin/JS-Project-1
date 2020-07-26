f226e303e9d8a253db88882e7c638029c8339f2f
Andrei Kudrin <andreikudrin@gmail.com>
2020-07-26 20:29:28 +0100

    JS Project 1 - CSS - SASS - SCSS

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Index.css";
 import ReactDOM from "react-dom";
 import MyComponentFunction from "./MyComponentFunction";
 import MyComponentClass from "./MyComponentClass";
