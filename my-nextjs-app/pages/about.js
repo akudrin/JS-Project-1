930d40561eab1a7e7c9c67e451d3c67140c9124a
Andrei Kudrin <andreikudrin@gmail.com>
2020-07-27 21:49:13 +0100

    JS Project 1 - Routing in Next.js

---
+++ my-nextjs-app/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+function About() {
+  return (
+    <div>
+      This is an application built using next.js to demonstrate the
+      effectiveness of server-side rendering!
+    </div>
+  );
+}
+export default About;
--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 function MyComponent() {
-  return <div>Hello from Next.js!</div>;
+  return (
+    <div>
+      <p>Hello from Next.js!</p>
+      <Link href="/About">
+        <a>About</a>
+      </Link>
+    </div>
+  );
 }
 export default MyComponent;
