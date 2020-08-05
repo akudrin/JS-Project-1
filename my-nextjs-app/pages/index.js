9f07a62c55cd04747b559703424f72b0c57a1c5e
Andrei Kudrin <andreikudrin@gmail.com>
2020-08-05 22:37:33 +0100

    JS Project 1 - Creating Interactive App Using Next.js

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -9,6 +9,9 @@ function MyComponent() {
       <Link href="/About">
         <a>About</a>
       </Link>
+      <Link href="/GithubUsers">
+        <a>GithubUsers</a>
+      </Link>
       <GetLink title="Page 1" Disp="page-1" Class="img p1"></GetLink>
       <GetLink title="Page 2" Disp="page-2" Class="img p2"></GetLink>
       <GetLink title="Page 3" Disp="page-3" Class="img p3"></GetLink>
