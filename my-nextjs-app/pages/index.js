9bbef3a53536e4008b780cdb7ef8868b6e12d4ed
Andrei Kudrin <andreikudrin@gmail.com>
2020-08-05 22:19:54 +0100

    JS Project 1 - Adding Multimedia Content Using CSS separate CSS file for all your styles

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import GetLink from "../SharedComponents/DynamicRouter";
+import "../styles.css";
 function MyComponent() {
   return (
     <div>
@@ -11,33 +12,6 @@ function MyComponent() {
       <GetLink title="Page 1" Disp="page-1" Class="img p1"></GetLink>
       <GetLink title="Page 2" Disp="page-2" Class="img p2"></GetLink>
       <GetLink title="Page 3" Disp="page-3" Class="img p3"></GetLink>
-      <style jsx global>
-        {`
-          a {
-            color: blue;
-          }
-          .img {
-            height: 50px;
-            width: 50px;
-            background-size: cover !important;
-            background-repeat: no-repeat !important;
-            background-position: center !important;
-            border: 1px solid black;
-            border-radius: 10px;
-            display: inline-block;
-            margin-top: 10px;
-          }
-          .p1 {
-            background: url(../static/Images/img1.jpg);
-          }
-          .p2 {
-            background: url(../static/Images/img2.jpg);
-          }
-          .p3 {
-            background: url(../static/Images/img3.jpg);
-          }
-        `}
-      </style>
     </div>
   );
 }
