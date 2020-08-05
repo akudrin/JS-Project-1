fc40d370c895769ce4874f6909b7094208d6b634
Andrei Kudrin <andreikudrin@gmail.com>
2020-08-05 22:13:34 +0100

    JS Project 1 - Adding Multimedia Content Using CSS in index.js

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -8,9 +8,36 @@ function MyComponent() {
       <Link href="/About">
         <a>About</a>
       </Link>
-      <GetLink title="Page 1" Disp="page-1"></GetLink>
-      <GetLink title="Page 2" Disp="page-2"></GetLink>
-      <GetLink title="Page 3" Disp="page-3"></GetLink>
+      <GetLink title="Page 1" Disp="page-1" Class="img p1"></GetLink>
+      <GetLink title="Page 2" Disp="page-2" Class="img p2"></GetLink>
+      <GetLink title="Page 3" Disp="page-3" Class="img p3"></GetLink>
+      <style jsx global>
+        {`
+          a {
+            color: blue;
+          }
+          .img {
+            height: 50px;
+            width: 50px;
+            background-size: cover !important;
+            background-repeat: no-repeat !important;
+            background-position: center !important;
+            border: 1px solid black;
+            border-radius: 10px;
+            display: inline-block;
+            margin-top: 10px;
+          }
+          .p1 {
+            background: url(../static/Images/img1.jpg);
+          }
+          .p2 {
+            background: url(../static/Images/img2.jpg);
+          }
+          .p3 {
+            background: url(../static/Images/img3.jpg);
+          }
+        `}
+      </style>
     </div>
   );
 }
